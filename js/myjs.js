bbf6c11fad039e8090780c7a2344a2d1bc9bad33
mattattaq <you@example.com>
2016-11-01 23:28:31 -0400

    fixed header, it wouldn't toggle open

--- js/myjs.js
+++ js/myjs.js
@@ -1,13 +1,8 @@
 $(function(){
   console.log('myJs works!');
-  $('.hamburger').on('click', function(){
-    $('.menu').toggleClass('open');
-    console.log('fired!');
-  }),
-  console.log('this injection works!');
   var includes = $('.include');
-  jQuery.each(includes, function(){
-    var file = 'views/' + $(this).data('include') + '.html';
-    $(this).load(file);
+    jQuery.each(includes, function(){
+      var file = 'views/' + $(this).data('include') + '.html';
+      $(this).load(file);
+    });
   });
-});
