5b0d6d09bf943f224d4ba98991b3e7afd173d4e2
MattAttaq <mattatttaq@gmail.com>
2016-11-27 16:21:48 -0500

    Added to Scout-App.io to projects (#35)
    * updated the blog images
    
    * added Scout-App.io to projects tab

--- js/projects_info/projects_info.js
+++ js/projects_info/projects_info.js
@@ -1,6 +1,6 @@
 var projects = [];
 
-/* 
+/*
 To add a new project, add a new item to blogsInfo and push that item to projects.
 */
 
@@ -11,8 +11,16 @@ var projectsInfo = {
     'link': 'https://gavsdevblog.wordpress.com/',
     'description': "New Description",
     'languages': ["HTML", "CSS", "JS"]
+  },
+  'scoutApp': {
+    'name': "Scout-App",
+    'imageLocation': './images/project_images/scout-app.jpg',
+    'link': 'http://scout-app.io/',
+    'description': "Scout-App, the easiest way to use Sass!",
+    'languages': ["jQuery", "JS", "Node", "sass", "HTML"]
   }
 };
 
 
 projects.push(new project(projectsInfo.testProject));
+projects.push(new project(projectsInfo.scoutApp));
