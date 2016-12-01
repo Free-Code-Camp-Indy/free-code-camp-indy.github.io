d70bff889a4738f311a14b248a97c08a5b8d4e5b
Gavyn Leavitt <firearrow5235@gmail.com>
2016-12-01 12:05:20 -0500

    Fixed issue with projectsInfo

--- js/projects_info/projects_info.js
+++ js/projects_info/projects_info.js
@@ -4,16 +4,16 @@ var projects = [];
 To add a new project, add a new item to blogsInfo and push that item to projects.
 */
 
-var projectsInfo = {
-  'scoutApp': {
+var projectsInfo = [
+  {
     'name': "Scout-App",
     'imageLocation': './images/project_images/scout-app.jpg',
     'link': 'http://scout-app.io/',
     'description': "Scout-App, the easiest way to use Sass!",
     'languages': ["jquery", "javascript", "nodejs", "sass", "html5"]
   }
-};
+];
 
-
-projects.push(new project(projectsInfo.testProject));
-projects.push(new project(projectsInfo.scoutApp));
+for (var i = 0; i < projectsInfo.length; i++){
+  projects.push(new project(projectsInfo[i]));
+}
