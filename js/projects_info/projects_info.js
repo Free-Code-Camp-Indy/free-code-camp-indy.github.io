a7ab5c23fb9e235c64011297a30dddf8c7dad8f5
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2016-12-09 11:32:45 -0500

    Update projects_info.js

--- js/projects_info/projects_info.js
+++ js/projects_info/projects_info.js
@@ -7,7 +7,7 @@ To add a new project, add a new item to blogsInfo and push that item to projects
 var projectsInfo = [
   {
     'name': "Scout-App",
-    'imageLocation': './images/project_images/scout-app.jpg',
+    'imageLocation': './images/project_images/scout-app.svg',
     'link': 'http://scout-app.io/',
     'description': "Scout-App, the easiest way to use Sass!",
     'languages': ["jquery", "javascript", "nodejs", "sass", "html5"]
