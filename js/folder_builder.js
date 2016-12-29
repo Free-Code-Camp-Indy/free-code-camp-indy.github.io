a15b2010e58347241f68c22bb05a2cfeadcde89e
James Maupin <jmsMaupinRC@gmail.com>
2016-12-29 18:31:10 -0500

    Create Initial design of Gallery folders

---
+++ js/folder_builder.js
@@ -0,0 +1,8 @@
+// A lot of inspiration from Gavyn Leavitt
+function build(folders){
+	folders.forEach(function(e){
+		$(".gallery-folders").append(new Folder(e).build());
+	})
+}
+
+build(folderInfo)
---
+++ js/folders/folder.js
@@ -0,0 +1,21 @@
+function Folder(params){
+	this.image = params.image;
+	this.location = params.location;
+	this.description = params.description;
+
+	this.build = function(){
+		return _.template(
+			'<a href="#">' +
+				'<div class="folder">' +
+					'<div class="img-container">' +
+						'<img class="img-responsive" alt="presentation" src="<%= image %>"/>' +
+					'</div>' +
+					'<div class="folder-desc">' +
+						'<h2><%= location %></h2>' +
+						'<p><%= description %></p>' +
+					'</div>' +
+				'</div>' +
+			'</a>'
+		)(this);
+	}
+}
