371df5824c42f1171ad2e032e975b4a9d9a23ed7
James Maupin <jmsMaupinRC@gmail.com>
2016-12-30 01:02:58 -0500

    Begin Work on Masonry-styled gallery

--- js/folders/folder.js
+++ js/folders/folder.js
@@ -1,11 +1,12 @@
 function Folder(params){
+	this.href = params.href;
 	this.image = params.image;
 	this.location = params.location;
 	this.description = params.description;
 
 	this.build = function(){
 		return _.template(
-			'<a href="#">' +
+			'<a href="<%= href %>">' +
 				'<div class="folder">' +
 					'<div class="img-container">' +
 						'<img class="img-responsive" alt="presentation" src="<%= image %>"/>' +
---
+++ js/gallery/gallery.js
@@ -0,0 +1,1 @@
+$('.grid').masonry( { itemSelector: '.item' } );
