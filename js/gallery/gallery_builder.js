3fe310ca88a503c0ad9b9725b0c08e0022603493
James Maupin <jmsMaupinRC@gmail.com>
2017-01-18 18:48:56 -0500

    Add lightbox when clicking on an image

--- js/gallery/gallery_builder.js
+++ js/gallery/gallery_builder.js
@@ -1,6 +1,6 @@
 $(function(){
 	
-	//append all images to .grid and then initialize isotope
+	//append all images to .grid and then initialize isotope`
 
 	var grid = $('.grid').imagesLoaded(function(){
 		$('.grid').isotope({
@@ -17,5 +17,20 @@ $(function(){
 		var filterVal = $(this).attr('data-val');
 		console.log(filterVal);
 		grid.isotope({filter: filterVal});
+	})
+
+	$('img').click(function(e){
+		$('.lightbox-content img').attr('src', e.target.src);
+		$('.lightbox').show();
+	})
+
+	$('body').click(function(e){
+		if(e.target.id === 'empty-space' || e.target.id === 'exit')
+			$('.lightbox').hide();
+	})
+
+	$('body').keyup(function(e){
+		if(e.key == 'Escape')
+			$('.lightbox').hide();
 	})
 })
