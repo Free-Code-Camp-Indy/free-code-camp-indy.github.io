da5a6fabf16f6b57e24cb63058fb2b3a288dea36
James Maupin <jmsMaupinRC@gmail.com>
2017-01-19 22:30:35 -0500

    Add some more example filters per image

--- js/gallery/images/image_info.js
+++ js/gallery/images/image_info.js
@@ -1,22 +1,22 @@
 var images = [
 	{
 		src: "https://a248.e.akamai.net/secure.meetupstatic.com/photos/event/9/5/4/f/highres_452798223.jpeg",
-		filter: "cyberia", 
+		filter: "cyberia talk", 
 		label : "Gwen giving a talk"
 	},
 	{
 		src: "https://a248.e.akamai.net/secure.meetupstatic.com/photos/event/7/0/1/b/highres_454708699.jpeg",
-		filter: "ironyard",
+		filter: "ironyard hackathon",
 		label: "a group at FCC Indy's first hackathon"
 	},
 	{
 		src: "https://a248.e.akamai.net/secure.meetupstatic.com/photos/event/7/0/3/5/highres_454708725.jpeg",
-		filter: "ironyard",
+		filter: "ironyard hackathon",
 		label: "Another group working on FCC Indy's hackathon"
 	},
 	{
 		src: "https://a248.e.akamai.net/secure.meetupstatic.com/photos/event/7/0/7/e/highres_454708798.jpeg",
-		filter: "ironyard",
+		filter: "ironyard hackathon",
 		label: "A third group working on FCC Indy's hackathon"
 	},
 	{
@@ -31,17 +31,17 @@ var images = [
 	},
 	{
 		src: "https://a248.e.akamai.net/secure.meetupstatic.com/photos/event/3/8/9/e/highres_455714494.jpeg",
-		filter: "bureau",
+		filter: "bureau talk",
 		label: "Campers listening to a talk at The Bureau"
 	},
 	{
 		src: "https://a248.e.akamai.net/secure.meetupstatic.com/photos/event/4/c/5/3/highres_455719539.jpeg",
-		filter: "bureau",
+		filter: "bureau talk",
 		label: "More campers at the Bureau"
 	},
 	{
 		src: "https://a248.e.akamai.net/secure.meetupstatic.com/photos/event/d/9/9/1/highres_452095697.jpeg",
-		filter: "cyberia",
+		filter: "cyberia talk",
 		label: "FOrrest giving a talk at Club Cyberia"
 	},
 	
