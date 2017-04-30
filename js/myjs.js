4b557a71a80c89a3dd823a732628d9ce209e687a
gwenf <gwenfaraday@gmail.com>
2017-04-30 02:29:17 -0400

    adding google analytics

--- js/myjs.js
+++ js/myjs.js
@@ -1,8 +1,7 @@
 $(function(){
-  console.log('myJs works!');
-  var includes = $('.include');
-    jQuery.each(includes, function(){
-      var file = 'views/' + $(this).data('include') + '.html';
-      $(this).load(file);
-    });
-  });
+	var includes = $('.include');
+	jQuery.each(includes, function(){
+		var file = 'views/' + $(this).data('include') + '.html';
+		$(this).load(file);
+	});
+});
--- scripts/myjs.js
+++ scripts/myjs.js
@@ -1,1 +1,1 @@
-$(function(){console.log("myJs works!");var i=$(".include");jQuery.each(i,function(){var i="views/"+$(this).data("include")+".html";$(this).load(i)})});
+$(function(){var i=$(".include");jQuery.each(i,function(){var i="views/"+$(this).data("include")+".html";$(this).load(i)})});
